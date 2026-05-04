43052272e3b08fb06dffeb956107fcac4488f614
Sebastian Kranz <sebkranz@gmail.com>
2026-05-04 08:01:16 +0200

    Initial commit

---
+++ R/rrd.R
@@ -0,0 +1,12 @@
+rrd_opts = function(log_regcheck=TRUE, log_all_err=TRUE, log_all=FALSE) {
+  as.list(environment())
+}
+
+
+rrd_md_do_logs = function(project_dir, parcels=list(), outfile="do_log.md") {
+  outdir = file.path(project_dir, "reports/do_log")
+  outfile =
+
+
+  outfile = file.path(project_dir, )
+}
